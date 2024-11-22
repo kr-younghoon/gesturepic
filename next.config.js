da37f77688f34922e345c2ba92f1dd715fc0ae50
kr-younghoon <sullanta0802@gmail.com>
2024-11-23 04:47:19 +0900

    build(config): update next.config.js for static export

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
   images: {
+    unoptimized: true,
     domains: ['kaipqjktuazebdqqmcjs.supabase.co'],
   },
+  distDir: 'dist',
 }
 
 module.exports = nextConfig
