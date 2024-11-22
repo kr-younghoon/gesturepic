81db9d0a2d7defad9ccfa550c71d44e84dfeb3f4
kr-younghoon <sullanta0802@gmail.com>
2024-11-23 04:44:41 +0900

    build(config): add next.config.js and update dependencies

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    domains: ['kaipqjktuazebdqqmcjs.supabase.co'],
+  },
+}
+
+module.exports = nextConfig
