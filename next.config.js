de33ae92185bd8bf2c87ba342a14f04b2f2068f2
kr-younghoon <sullanta0802@gmail.com>
2024-11-23 04:57:51 +0900

    fix(config): remove static export for dynamic auth support

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
-    unoptimized: true,
     domains: ['kaipqjktuazebdqqmcjs.supabase.co'],
   },
-  distDir: 'dist',
 }
 
 module.exports = nextConfig
