5632cf6df6666cb6b7025755ec913a9dc9f257cc
kr-younghoon <sullanta0802@gmail.com>
2024-11-24 02:37:31 +0900

    chore: Update dependencies and Next.js configuration
    - Update package.json dependencies
    - Update package-lock.json
    - Update Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['kaipqjktuazebdqqmcjs.supabase.co'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'kaipqjktuazebdqqmcjs.supabase.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
   },
 }
 
